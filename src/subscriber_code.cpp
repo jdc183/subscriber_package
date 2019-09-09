5df986acb705ae52a79a1522baf6940b1dcaa751
jdc183 <jdc@case.edu>
2019-09-09 15:28:38 -0400

    Initial commit

---
+++ src/subscriber_code.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+//Method called upon receipt of message
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+	//Initialize the ros node
+	ros::init(argc, argv, "subscriber_node");
+	ros::NodeHandle n;
+	
+	//Set up the chatterCallback to handle the event
+	ros::Subscriber sub = n.subscribe("publisher_topic", 1000, chatterCallback);
+	
+	//Pump the callbacks!
+	ros::spin();
+	
+	return 0;
+}
